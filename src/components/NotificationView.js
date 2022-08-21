dc4d42fe7e64be9990c945798d18a39278203158
Aleja Ocampo <omaira.527@hotmail.com>
2022-08-20 20:40:45 -0500

    Se agrega nuevos componentes

---
+++ src/components/NotificationView.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const Notifications = () => {
+  const handleNotification = () => {};
+
+  return (
+    <>
+      <button onClick={handleNotification}>Notificacion</button>
+    </>
+  );
+};
+
+export default Notifications;
